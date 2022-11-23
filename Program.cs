28a8f5486bc392b49c545f49be753b3417aa65ce
MaSlaff <maslaff@yandex.ru>
2022-11-23 22:25:42 +0300

    Task solved

--- Program.cs
+++ Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] fillArray = { "hello", "2", "world", ":-)" };
+string[] sourceArray;
+string[] newArray;
+int maxWordLength = 3;
+int maxArrayLength = 30;
+
+if (!GetArray(out sourceArray, maxArrayLength)) sourceArray = fillArray;
+GetFilteredArray(sourceArray, out newArray, maxWordLength);
+PrintArray(sourceArray);
+PrintArray(newArray);
+
+int GetFilteredArray(string[] srcArr, out string[] newArr, int maxLen)
+{
+    int count = 0;
+
+    foreach (var el in srcArr)
+        if (el.Length <= maxLen) count++;
+
+    newArr = new string[count];
+
+    for (int i = 0, j = 0; i < srcArr.Length; i++)
+        if (srcArr[i].Length <= maxLen) newArr[j++] = srcArr[i];
+
+    return count;
+}
+
+bool GetArray(out string[] arr, int max)
+{
+    arr = new string[max];
+    int len = 0;
+
+    Console.WriteLine($"Для автозаполнения нажмите Enter, иначе введите строки (Max {max}). Нажимайте Enter для ввода следующей. Для завершения оставьте строку пустой и нажмите Enter.");
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        string? str = Console.ReadLine();
+        if (str == null || str == "") { len = i; break; }
+        arr[i] = str;
+    }
+
+    if (len == 0)
+    {
+        arr = new string[0];
+        return false;
+    }
+
+    if (len == arr.Length)
+        Console.WriteLine($"Достигнут предел заполнения {max}");
+    else Array.Resize(ref arr, len);
+
+    return true;
+}
+
+void PrintArray(string[] arr, string sep = ", ")
+{
+    Console.Write($"[\"{arr[0]}\"");
+    for (int i = 1; i < arr.Length; i++)
+        Console.Write($"{sep}\"{arr[i]}\"");
+    Console.WriteLine("]");
+}
